bc528d2fc32a49c4c68e4b6e1cf12e54a2c10b6b
spencerbarriball <sbarriball@me.com>
2018-03-21 12:50:55 +0000

    initial Commit

--- assets/js/graph.js
+++ assets/js/graph.js
@@ -1,5 +1,5 @@
 queue()
-    .defer(d3.csv, "assets/csv/lifeExpectancyData.csv")
+    .defer(d3.csv, "assets/csv/WHOData.csv")
     .await(makeGraphs);
 
 
@@ -7,34 +7,129 @@ function makeGraphs(error, worldData) {
     var ndx = crossfilter(worldData);
 
     show_country_selector(ndx);
-    show_percentage_of_develop_countries(ndx, "Developing", "#percent-of-developing-countries");
-    show_percentage_of_develop_countries(ndx, "Developed", "#percent-of-developed-countries");
+    show_status_balance(ndx);
+    show_average_life_expectancy(ndx);
+     show_average_adultMortality(ndx)
 
     dc.renderAll();
 }
 
-
-function show_country_selector(ndx) {
-    var countryDim = ndx.dimension(dc.pluck("Country"));
+function show_country_selector(ndx)
+{
+    var countryDim = ndx.dimension(
+        function (p) { return p.Country;
+    });
     var countrySelect = countryDim.group();
-
-    dc.selectMenu("#country-selector")
+    var select = dc.selectMenu("#country-selector")
         .dimension(countryDim)
         .group(countrySelect);
+    select.title(function (d) {
+        return d.key;
+    })
 }
 
-function show_percentage_of_develop_countries(ndx, dStatus, element) {
-    var total = 0;
-    var state = 0;
-    var percentThatAreDevelopingCountries = ndx.groupAll().reduce(
+function show_status_balance(ndx) {
+    var statusDim = ndx.dimension(
+        function (p) { return p.Status;
+        });
+    var statusMix = statusDim.group();
+
+    dc.pieChart("#status-balance-pie")
+        .dimension(statusDim)
+        .group(statusMix)
+        .cx(195)
+        .cy(175)
+        .drawPaths(true)
+        .innerRadius(40)
+        .radius(150)
+        .transitionDuration(750);
+}
+
+function show_average_life_expectancy(ndx) {
+    var statusDim = ndx.dimension(dc.pluck("Status"));
+    var averageLifeExpectancyByStatus = statusDim.group().reduce(
         function (p, v) {
-            if (v.Status === dStatus) {
-                p.count++;
+            p.count++;
+            p.total += parseInt(v.LifeExpectancy);
+            return p;
+        },
+        function (p, v) {
+            p.count--;
+            if (p.count == 0) {
+                p.total = 0;
+            } else {
+                p.total -= parseInt(v.LifeExpectancy);
             }
-        total += 0;
-    return (state / total) * 100;
+            return p;
+        },
+        function () {
+            return {count: 0, total: 0};
         }
     );
+
+    dc.barChart("#average-Life-expectancy")
+        .width(350)
+        .height(350)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(statusDim)
+        .group(averageLifeExpectancyByStatus)
+        .valueAccessor(function (d) {
+            if (d.value.count == 0) {
+                return 0;
+            } else {
+                return d.value.total / d.value.count;
+            }
+        })
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("Country classification")
+        .yAxisLabel("Life Expectancy Age")
+        .yAxis().ticks(20);
 }
 
+function  show_average_adultMortality(ndx) {
+    var statusDim = ndx.dimension(dc.pluck("Status"));
+    var averageAdultMortalityByStatus = statusDim.group().reduce(
+        function (p, v) {
+            p.count++;
+            p.total += parseInt(v.AdultMortality);
+            return p;
+        },
+        function (p, v) {
+            p.count--;
+            if (p.count == 0) {
+                p.total = 0;
+            } else {
+                p.total -= parseInt(v.AdultMortality);
+            }
+            return p;
+        },
+        function () {
+            return {count: 0, total: 0};
+        }
+    );
+
+    dc.barChart("#average-adult-mortality")
+        .width(350)
+        .height(350)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(statusDim)
+        .group(averageAdultMortalityByStatus)
+        .valueAccessor(function (d) {
+            if (d.value.count == 0) {
+                return 0;
+            } else {
+                return d.value.total / d.value.count;
+            }
+        })
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("Country classification")
+        .yAxisLabel("Adult Mortality per Thousand")
+        .yAxis().ticks(20);
+}
 
